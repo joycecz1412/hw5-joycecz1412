bc755eec442273bc7e32c6d67210c43bfd85c739
joycecz1412 <149197895+joycecz1412@users.noreply.github.com>
2024-04-30 01:34:51 -0500

    Update pa5.py

--- pa5.py
+++ pa5.py
@@ -2,14 +2,34 @@
 
 def gcd(a, b):
   if a >= b:
-    continue
+    pass 
   else:
     a, b = b, a #swap values if a < b
   if b == 0:
     return a #base case
-  while b!= 0:
-    gcd(a,b) = gcd(b, a % b) 
-    return a 
+  return gcd(b, a % b)
+
+def remove_pairs(string):
+  if 'EW' not in string and 'WE' not in string and \
+    'NS' not in string and 'SN' not in string:
+    return string
+  if 'EW' not in string[0:2] and 'WE' not in string[0:2] and \
+    'NS' not in string[0:2] and 'SN' not in string[0:2]:
+    return remove_pairs(string[0]) + remove_pairs(string[1:])
+  else:
+    return remove_pairs(string[2:])
+  
+def bisection_root(func, x1, x2):
+  if (-0.001 <= func(x1) <= 0.001):
+    return x1
+  elif (-0.001 <= func(x2) <= 0.001):
+    return x2
+  else:
+    new_x = x1 + abs(x1-x2)/2
+    if (func(new_x) * func(x1)) > 0:
+      return bisection_root(func, new_x, x2)
+    else:
+      return bisection_root(func, x1, new_x)
 
   
   
