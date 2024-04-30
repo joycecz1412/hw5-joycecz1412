f7f0ad96561329ed4117a05ed20bcd65dadfb458
joycecz1412 <149197895+joycecz1412@users.noreply.github.com>
2024-04-30 01:44:10 -0500

    Update pa5.py

--- pa5.py
+++ pa5.py
@@ -1,6 +1,8 @@
 """HW5 Code"""
 
 def gcd(a, b):
+  '''Greatest common divisor'''
+  
   if a >= b:
     pass 
   else:
@@ -10,26 +12,27 @@ def gcd(a, b):
   return gcd(b, a % b)
 
 def remove_pairs(string):
+  '''Remove u-turns in direction'''
+  
   if 'EW' not in string and 'WE' not in string and \
-    'NS' not in string and 'SN' not in string:
+  'NS' not in string and 'SN' not in string:
     return string
   if 'EW' not in string[0:2] and 'WE' not in string[0:2] and \
-    'NS' not in string[0:2] and 'SN' not in string[0:2]:
+  'NS' not in string[0:2] and 'SN' not in string[0:2]:
     return remove_pairs(string[0]) + remove_pairs(string[1:])
   else:
     return remove_pairs(string[2:])
   
 def bisection_root(func, x1, x2):
-  if (-0.001 <= func(x1) <= 0.001):
-    return x1
-  elif (-0.001 <= func(x2) <= 0.001):
-    return x2
+  '''find root of function using bisection method'''
+  
+  if (-0.001 < func(x1) < 0.001):
+    return round(x1,2)
+  elif (-0.001 < func(x2) < 0.001):
+    return round(x2,2)
   else:
     new_x = x1 + abs(x1-x2)/2
     if (func(new_x) * func(x1)) > 0:
       return bisection_root(func, new_x, x2)
     else:
       return bisection_root(func, x1, new_x)
-
-  
-  
